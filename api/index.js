516d6d0b7e664698edbb12d7a6c7a58ec918d6de
Kongara Rahul <rahul.kongara13@gmail.com>
2025-01-29 23:31:34 +0530

    Update cors methods l:34 index.js

--- api/index.js
+++ api/index.js
@@ -31,6 +31,7 @@ app.use(cors({
             callback(new Error("Not allowed by CORS"));
         }
     },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
 app.use(express.json());
